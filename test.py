57a953f3f8c37d5c8da7981e0a4af0e689c59c23
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-11-22 23:21:10 -0600

    Make function to open segmented images

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+"""
+Run this file to make sure the git/image stuff is all good
+"""
+from scipy.misc import imread
 import matplotlib.pyplot as plt
+
+
+def open_im(path):
+    im = imread(path)
+    plt.imshow(im)
+    plt.show()
+
+
+# Seeing if stuff works
+open_im('./annotations/trimaps/Abyssinian_1.png')
