856d05a450683fa5c5bfb11101da12135222de1c
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-04 15:58:13 -0600

    Clean up code

--- Colonel_Thomas.py
+++ Colonel_Thomas.py
@@ -1,20 +1,19 @@
-import numpy as np
-from sklearn.svm import SVM
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 28 17:16:35 2016
 
 @author: ChristopherChee
 """
-
-"""
-"Kernel" SVM method for training data.
-@params matrices: a set of data matrices, one for each image
-@return SVM: kernel SVM object for given data matrices
-"""
+import numpy as np
+from sklearn.svm import SVM
 
 
 def Colonel_Thomas(matrices):
+    """
+    Method to form the data matrix for SVM
+    @params matrices: a list of data matrices, one for each image
+    @return data: a matrix of stacked data points
+    """
     # determine size of data matrix (spliced matrices)
     length = 0
     for matrix in matrices:
@@ -30,6 +29,11 @@ def Colonel_Thomas(matrices):
 
 
 def Train_Thomas(training_data):
+    """
+    "Kernel" SVM method for training data.
+    @params training_data: a matrix of stacked data points
+    @return SVM: kernel SVM object for given data matrices
+    """
     kernel = SVM()
     model = kernel.fit(training_data[0:5], training_data[[6]])
     return model
--- EmotionalSupportVectorMachine.py
+++ EmotionalSupportVectorMachine.py
@@ -8,36 +8,13 @@ from get_images import *
 from bobo import *
 from Colonel_Thomas import *
 
-from sklearn import svm
-#from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix
-#from sklearn.preprocessing import scale
-#from sklearn.svm.libsvm import predict
-import scipy
-import numpy
-
-# change the path to whatever you need it to be
-spiral = scipy.io.loadmat(
-    'C:/Users/Joseph/Documents/ELEC 301/Homework 11/TwoSpirals.mat')
-labels = spiral['y']
-data = spiral['X']
-train = svm.SVC()
-train.fit(data, labels.ravel())
-conf = confusion_matrix(train.predict(data), labels)
-# kf = KFold(n_splits=5, shuffle=True)
-# kf.get_n_splits(data)
-# conf = numpy.zeros((2,2))
-# conf = numpy.add(conf, (1.0/5.0)*confusion_matrix(train.predict(data), labels))
-"""
-for train_index, test_index in kf.split(data):
-    train = svm.SVC()
-    train.fit(data[train_index], labels[train_index].ravel())
-    y_pred = train.predict(data[test_index])
-    y_test = labels[test_index]
-    conf = numpy.add(conf, (1.0/5.0)*confusion_matrix(y_pred, y_test))
-"""
-print conf
-print 'Error: ', (conf[0, 1] + conf[1, 0]) / conf.sum()
+# from sklearn import svm
+# from sklearn.model_selection import KFold
+# from sklearn.metrics import confusion_matrix
+# from sklearn.preprocessing import scale
+# from sklearn.svm.libsvm import predict
+# import scipy
+# import numpy
 
 # Testing
 full_list = get_image_list('/annotations/list.txt')
