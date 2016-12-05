803f8c6c964ffb37894193d092fb058bbde457ac
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-04 23:50:57 -0600

    Make function to reconstruct
    By Ben

---
+++ invBob.py
@@ -0,0 +1,32 @@
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+def inv_Bob(lis,frow,fcol):
+
+	row = 0
+	col = 0
+	image = np.zeros(frow,fcol)
+	for r in lis.shape[0]:
+		image[row][col] = lis[r]
+		if(col<fcol):
+			col = col + 1
+		else:
+			row = row + 1
+			col = 0
+	return image
+
+def inv_col(labels,n):
+	done = 0
+	seperate = []
+	for ni in n:
+		seperate.append(labels[done:done+ni])
+		done = done + ni
+	return seperate
+
+
+def disp(image):
+	plt.imshow(image, interpolation='nearest')
+	plt.show()
+	return
+
