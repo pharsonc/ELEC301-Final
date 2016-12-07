4b9e743ca8e8de875193503c8c62e8b8802cf3cc
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-07 14:21:47 -0600

    Change test code
    Utilize load/save functions to reduce future processing of training data

--- EmotionalSupportVectorMachine.py
+++ EmotionalSupportVectorMachine.py
@@ -17,25 +17,39 @@ from invBob import *
 # import scipy
 # import numpy
 
-# Testing
+########################### Testing ###########################
+"""
+	Running the code the first time will form the data matrix
+	and save it to 'training_data.dat'.
+
+	To run the SVM, comment out everything in step 1,
+	uncomment step 2, and run.
+"""
+#################### 1. FORMING DATA MATRIX ###################
 full_list = get_image_list('./annotations/list.txt')
 # full_list = full_list[:2]
 all_images = get_images(full_list, 1)
 all_labels = get_images(full_list, 0)
 data_matrix_arr = Bob(all_images, all_labels)
 training_data = Colonel_Thomas(data_matrix_arr)
-model = Train_Thomas(training_data)
 
-# Very hard-coded test
-# Predicting on first image in training data
-test_image = all_images[0]
-rows = test_image.shape[0]
-cols = test_image.shape[1]
-test_data_matrix = data_matrix_arr[0][:, 0:5]
-test_label = model.predict(test_data_matrix)
-# Reshaping etc
-# rows = 500
-# cols = 394
-# test_label = 2*np.ones((1, rows*cols))
-reshaped = retrieve_image(test_label, [rows * cols], rows, cols)
-disp(reshaped[0])
+# Saving data matrix to file
+save_matrix(training_data, 'training_data.dat')
+
+#################### 2. RUNNING SVM ###########################
+# test = load_matrix('training_data.dat')
+# model = Train_Thomas(training_data)
+
+# # Very hard-coded test
+# # Predicting on first image in training data
+# test_image = all_images[0]
+# rows = test_image.shape[0]
+# cols = test_image.shape[1]
+# test_data_matrix = data_matrix_arr[0][:, 0:5]
+# test_label = model.predict(test_data_matrix)
+# # Reshaping etc
+# # rows = 500
+# # cols = 394
+# # test_label = 2*np.ones((1, rows*cols))
+# reshaped = retrieve_image(test_label, [rows * cols], rows, cols)
+# disp(reshaped[0])
