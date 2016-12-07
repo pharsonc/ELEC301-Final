c8cadd4e968549a9ebe540aa31b75f8e9d405bfb
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-07 17:04:17 -0600

    Rewrite inv_Bob with numpy methods
    Update docstrings

--- invBob.py
+++ invBob.py
@@ -8,35 +8,25 @@ Functions to process the output of our SVM to obtain images
 
 def inv_Bob(lis, frow, fcol):
     """
-
-    :param lis: A list representing a column
-    :param frow: The number of rows in the final image
-    :param fcol: The number of columns in the final image
-    :return: A numpy matrix representing an image
+    Args:
+        lis (list): A list representing a column
+        frow (int): The number of rows in the final image
+        fcol (int): The number of columns in the final image
+    Returns:
+        image (np.matrix): A numpy matrix representing an image
     """
-    row = 0
-    col = 0
-    image = np.zeros((frow, fcol))
-    # for r in lis.shape[0]:
-    for r in range(len(lis)):		# Number of elements in column
-        # print row
-        # print col
-        # print fcol, frow
-        image[row][col] = lis[r]
-        if(col <= fcol - 2):
-            col = col + 1
-        else:
-            row = row + 1
-            col = 0
+    arr = np.array(lis)
+    image = arr.reshape((frow, fcol))
     return image
 
 
 def inv_col(labels, n):
     """
-
-    :param labels: An array of predicted labels for some number of classified images
-    :param n: An array whose ith element is the size (rows * cols) of the ith image
-    :return: A list of columns represented as lists
+    Args:
+        labels (np.ndarray): A 1D array of predicted labels for some number of classified images
+        n: An array whose ith element is the size (rows * cols) of the ith image
+    Returns:
+        separate (list): A list of columns represented as lists
     """
     done = 0
     separate = []
@@ -55,11 +45,13 @@ def disp(image):
 def retrieve_image(labels, n, frow, fcol):
     """
 
-    :param labels: An array of predicted labels for some number of classified images
-    :param n: An array whose ith element is the size (rows * cols) of the ith image
-    :param frow: The number of rows in the final image
-    :param fcol: The number of columns in the final image
-    :return: Displays image
+    Args:
+        labels (np.ndarray): A 1D array of predicted labels for some number of classified images
+        n (list): An array whose ith element is the size (rows * cols) of the ith image
+        frow (int): The number of rows in the final image
+        fcol (int): The number of columns in the final image
+    Returns:
+        im_lis (list): A list of numpy matrices representing images
     """
     lis = inv_col(labels, n)
     im_lis = []
