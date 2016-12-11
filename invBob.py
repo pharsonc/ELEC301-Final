83fbe776c7c627cad1ea8009bf9c62ba7eb95972
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-11 16:46:53 -0600

    Add image dimension stuff for reshaping

--- invBob.py
+++ invBob.py
@@ -42,20 +42,20 @@ def disp(image):
     return
 
 
-def retrieve_image(labels, n, frow, fcol):
+def retrieve_image(labels, im_dims):
     """
 
     Args:
-        labels (np.ndarray): A 1D array of predicted labels for some number of classified images
-        n (list): An array whose ith element is the size (rows * cols) of the ith image
-        frow (int): The number of rows in the final image
-        fcol (int): The number of columns in the final image
+        labels (np.ndarray): A 1D array of predicted labels for some number of images
+        im_dims (list): A list of tuples representing image dimension for each image
     Returns:
         im_lis (list): A list of numpy matrices representing images
     """
+    n = len(im_dims)
     lis = inv_col(labels, n)
     im_lis = []
-    for l in lis:
-        image = inv_Bob(l, frow, fcol)
+    for ind in range(n):
+        frow, fcol = im_dims[ind]
+        image = inv_Bob(lis[ind], frow, fcol)
         im_lis.append(image)
     return im_lis
