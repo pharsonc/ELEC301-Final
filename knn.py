61e05bbe23de2c1f8594906b474edc1703468de3
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-12 14:26:39 -0600

    Add KNN stuff

--- knn.py
+++ knn.py
@@ -1,1 +1,19 @@
-from sklearn.preprocessing import scale
+# import numpy as np
+# from scipy.misc import imread
+# from sklearn.preprocessing import scale
+from get_images import *
+
+full_list = get_image_list('./annotations/list.txt')
+image_zip = get_images3(full_list)
+
+# TO-DO: Scaling training images
+
+# Get test images
+test_images = get_test_images2()
+test_image_dims = get_image_dims(test_images)
+# TO-DO: Scale test images
+
+
+# Run K-NN
+
+# Scale labels to original size
