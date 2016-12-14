cbe8c90c76534e465175f9bc0d49bb8d89b4f72a
Pharson Chalermkraivuth <pharson.ch@gmail.com>
2016-12-13 23:03:17 -0600

    Add k-fold validation

--- EmotionalSupportVectorMachine.py
+++ EmotionalSupportVectorMachine.py
@@ -11,9 +11,8 @@ from invBob import *
 
 from scipy.misc import imsave
 from sklearn.externals import joblib
-# from sklearn import svm
-# from sklearn.model_selection import KFold
-# from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
 # from sklearn.preprocessing import scale
 # from sklearn.svm.libsvm import predict
 # import scipy
@@ -35,15 +34,15 @@ from sklearn.externals import joblib
 ################## 1. FORMING DATA MATRICES ###################
 # # TRAINING DATA
 # full_list = get_image_list('./annotations/list.txt')
-# full_list = full_list[0:200]
-# image_zip = get_images3(full_list)
+# # full_list = full_list[0:200]
+# image_zip = get_images(full_list)
 # data_matrix_arr = Bob2(image_zip)
 # training_data = Colonel_Thomas(data_matrix_arr)
 # #
 # # # Saving data matrices to file
-# # save_matrix(training_data, 'training_data.dat')
+# save_matrix(training_data, 'training_data.dat')
 
-#################### 2. MAKING MODEL  #########################
+# ################### 2. MAKING MODEL  #########################
 # training_data_ld = load_matrix('training_data.dat')
 # model = Train_Thomas(training_data_ld)
 # model = Train_Thomas(training_data)
@@ -51,43 +50,78 @@ from sklearn.externals import joblib
 # # Save model
 # joblib.dump(model, 'classifier.pkl')
 
-#################### 3a. TESTING MODEL  #######################
+# ################### 3a. CROSS-VALIDATION  ####################
+# RUN THIS BY ITSELF
+
+def dice(im1, im2):
+	im1 = np.asarray(im1).astype(np.bool)
+	im2 = np.asarray(im2).astype(np.bool)
+
+	if im1.shape != im2.shape:
+		raise ValueError("Shape mismatch")
+
+	# Compute Dice coefficient
+	intersection = np.logical_and(im1, im2)
+
+	return 2. * intersection.sum() / (im1.sum() + im2.sum())
+
+
+full_list = get_image_list('./annotations/list.txt')
+full_list = full_list[0:10]
+X, y, dims = get_images2(full_list)
+
+X = np.asarray(X)
+y = np.asarray(y)
+dims = np.asarray(dims)
+
+# K-fold cross-validation
+num_folds = 5
+kf = KFold(n_splits=num_folds)
+dice_coeffs = []
+
+for train_index, test_index in kf.split(X):
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
+	dims_train, dims_test = dims[train_index], dims[test_index]
+
+	data_matrix_arr = Bob2(zip(X_train, y_train))
+	data_matrix = Colonel_Thomas(data_matrix_arr)
+
+	model = Train_Thomas(data_matrix)
+
+	# Processing test data
+	test_data_matrix_arr = Bob2(zip(X_test, X_test))
+	test_data_matrix = Colonel_Thomas(test_data_matrix_arr)
+	test_data_matrix = test_data_matrix[:, 0:5]
+
+	test_label = model.predict(test_data_matrix)
+
+	ground_truth = retrieve_image(y_test, dims_test)
+	reshaped = retrieve_image(test_label, dims_test)
+
+	dice_coeff = 0.0
+	for i in range(len(reshaped)):
+		seg = reshaped[i]
+		gt = ground_truth[i]
+		dice_coeff += dice(seg, gt) / len(reshaped)
+	dice_coeffs.append(dice_coeff)
+
+print dice_coeffs
+
+# ################### 3b. TESTING MODEL  #######################
 # # Load model
 # model = joblib.load('classifier.pkl')
+
+# # Get test images
+# test_images, test_image_dims = get_test_images2()
 #
-# # Very hard-coded test
-# # Predicting on first image in training data
-# test_image = get_images(['Abyssinian_100'], 1)
-# rows = test_image[0].shape[0]
-# cols = test_image[0].shape[1]
-#
-# # Just want the data matrix (ignore labels column)
-# test_data_matrix = Bob(test_image, test_image)
-# test_data_matrix = test_data_matrix[0][:, 0:5]
+# test_data_arr = Bob(test_images, test_images)
+# test_data_matrix = Colonel_Thomas(test_data_arr)
+# test_data_matrix = test_data_matrix[:, 0:5]
 # test_label = model.predict(test_data_matrix)
 #
-# reshaped = retrieve_image(test_label, [(rows, cols)])
-# disp(reshaped[0])
+# reshaped = retrieve_image(test_label, test_image_dims)
 #
 # for n in range(len(reshaped)):
-# 	im = Image.fromarray(reshaped[n])
-# 	filename = 'segmented', n, '.png'
-# 	im.save(filename)
-
-#################### 3b. TESTING MODEL  #######################
-# Load model
-# model = joblib.load('classifier.pkl')
-
-# Get test images
-test_images, test_image_dims = get_test_images2()
-
-test_data_arr = Bob(test_images, test_images)
-test_data_matrix = Colonel_Thomas(test_data_arr)
-test_data_matrix = test_data_matrix[:, 0:5]
-test_label = model.predict(test_data_matrix)
-
-reshaped = retrieve_image(test_label, test_image_dims)
-
-for n in range(len(reshaped)):
-	filename = 'segmented' + str(n+1) + '.png'
-	imsave(filename, reshaped[n])
+# 	filename = 'segmented' + str(n+1) + '.png'
+# 	imsave(filename, reshaped[n])
